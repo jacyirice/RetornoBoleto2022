c7a022f9979cad8658943a4f208a50e1078c8278
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2022-04-11 09:27:30 -0300

    tornando metodo processarLinhaArquivo abstrato

--- src/retornoboleto/LeituraRetorno.java
+++ src/retornoboleto/LeituraRetorno.java
@@ -17,6 +17,6 @@ public interface LeituraRetorno {
     DateTimeFormatter FORMATO_DATA = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     DateTimeFormatter FORMATO_DATA_HORA = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
-    Boleto processarLinhaArquivo(String[] vetorLinhaArquivo);
-    
+    abstract Boleto processarLinhaArquivo(String[] vetorLinhaArq);
+
 }
